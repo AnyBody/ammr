4f7ce560ee25e2154ed8bccef20500a1eac079d5
Morten Enemark Lund <melund@gmail.com>
2017-10-12 20:43:46 +0200

    Add new mandible model and refactor existing mandible
    Also remove unused AnySurf3 files, and all STL files
    to save space

---
+++ Body/Mandible/NormalMandible_AAU/MandibleRefPoints.pp
@@ -0,0 +1,11 @@
+<!DOCTYPE PickedPoints>
+<PickedPoints>
+ <DocumentData>
+  <DateTime time="14:08:54" date="2013-02-07"/>
+  <User name="msa"/>
+  <DataFileName name="1172X De Zee, Mark_Union_1_001.stl"/>
+  <templateName name="MandibleRefPoints.pptpl"/>
+ </DocumentData>
+ <point x="42.4612" y="119.636" z="-55.1903" active="1" name="RightCondyle"/>
+ <point x="141.549" y="122.872" z="-57.6456" active="1" name="LeftCondyle"/>
+</PickedPoints>
---
+++ Body/Mandible/NormalMandible_AAU/SkullRefPoints.pp
@@ -0,0 +1,12 @@
+<!DOCTYPE PickedPoints>
+<PickedPoints>
+ <DocumentData>
+  <DateTime time="14:07:13" date="2013-02-07"/>
+  <User name="msa"/>
+  <DataFileName name="1172X De Zee, Mark_HeadErode5_001.stl"/>
+  <templateName name="SkullRefPoints.pptpl"/>
+ </DocumentData>
+ <point x="142.055" y="134.89" z="-54.7368" active="1" name="LeftEar"/>
+ <point x="128.203" y="61.8469" z="-57.8488" active="1" name="LeftEye"/>
+ <point x="35.762" y="133.938" z="-51.3871" active="1" name="RightEar"/>
+</PickedPoints>
