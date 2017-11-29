56e137c4cd760f0553d5dd9b79d8dc8ac5115976
Morten Enemark Lund <melund@gmail.com>
2017-11-29 23:30:21 +0100

    Update BVH model for Xsens webcast

--- Docs/Applications/Mocap/plot_BVH.py
+++ Docs/Applications/Mocap/plot_BVH.py
@@ -7,7 +7,7 @@ Example of a MoCap mdoel using data from an inertial motional caputure suit.
 The model uses a BVH file with data from an Xsens suit. The ground reaction
 forces are predicted using the GRF prediction algorithm. 
     
-``Application/MocapExamples/BVH_Xsens/Trials/Walk/Main.any``
+``Application/MocapExamples/BVH_Xsens/Trials/BVH01/Main.any``
 
 """
 import sys
