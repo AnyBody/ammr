f60ef348216e7d9cae31ce78acc07828c6bc6378
Morten Enemark Lund <melund@gmail.com>
2018-03-14 09:03:04 +0100

    Add gallery entry for squat example

---
+++ Docs/Applications/ADLs_and_ergonomics/plot_Squat.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+r"""
+Squat demo
+===============================
+
+This model demonstrates parameterized squating motion.
+
+**Main file:** ``Application/Examples/Squat/Squat.main.any``
+
+.. add more detailed description here
+
+
+"""
+
+
+import sys
+sys.path.insert(0, '../../exts')
+import gallery
+
+gallery.plot("../images/Squat.jpg")
+gallery.show()
