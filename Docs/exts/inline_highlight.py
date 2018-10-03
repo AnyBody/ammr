7a2ba42556754c2ab45f4ebb912ab0a2d1f7e37a
Morten Enemark Lund <melund@gmail.com>
2018-10-03 14:31:57 +0200

    Format python files with black

--- Docs/exts/inline_highlight.py
+++ Docs/exts/inline_highlight.py
@@ -4,19 +4,19 @@
 # https://bitbucket.org/klorenz/sphinxcontrib-inlinesyntaxhighlight/
 #
 # Copyright (c) 2013, Kay-Uwe (Kiwi) Lorenz
-# 
+#
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright
 #   notice, this list of conditions and the following disclaimer.
-# 
+#
 # * Redistributions in binary form must reproduce the above copyright
 #   notice, this list of conditions and the following disclaimer in the
 #   documentation and/or other materials provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -34,62 +34,64 @@ from docutils import nodes
 
 from sphinx.writers.html import HTMLTranslator
 
-DIV_PRE_RE = re.compile(r'^<div[^>]*><pre>')
-PRE_DIV_RE = re.compile(r'\s*</pre></div>\s*$')
+DIV_PRE_RE = re.compile(r"^<div[^>]*><pre>")
+PRE_DIV_RE = re.compile(r"\s*</pre></div>\s*$")
 
 
 def html_visit_literal(self, node):
 
     shall_highlight = False
-    
-    code_keyword = 'code' in node['classes']
+
+    code_keyword = "code" in node["classes"]
     anyscript_keyword = any(
-        v in [c.lower() for c in node['classes']]
-        for v in ('anyscript', 'anyscriptdoc')
+        v in [c.lower() for c in node["classes"]] for v in ("anyscript", "anyscriptdoc")
     )
 
-    if node.rawsource.startswith('``') or code_keyword or anyscript_keyword:
+    if node.rawsource.startswith("``") or code_keyword or anyscript_keyword:
         if anyscript_keyword:
-            lang = 'AnyScriptDoc'
+            lang = "AnyScriptDoc"
         else:
             lang = self.highlightlang
 
-        highlight_args = node.get('highlight_args', {})
+        highlight_args = node.get("highlight_args", {})
 
         # determine if shall_highlight
         shall_highlight = True
 
         attrs = node.attributes
 
-        if 'role' in attrs:  # e.g. for :file:`...`
+        if "role" in attrs:  # e.g. for :file:`...`
             shall_highlight = False
 
-        elif 'code' in attrs['classes'] and (attrs.get('language') or lang):
+        elif "code" in attrs["classes"] and (attrs.get("language") or lang):
             shall_highlight = True
 
-        if not lang or lang == 'none':
+        if not lang or lang == "none":
             shall_highlight = False
 
     if shall_highlight:
         highlighted = self.highlighter.highlight_block(
-            node.astext(), lang, location=node, **highlight_args)
+            node.astext(), lang, location=node, **highlight_args
+        )
 
         # highlighted comes as <div class="highlighted"><pre>...</pre></div>
-        highlighted = DIV_PRE_RE.sub('', highlighted)
-        highlighted = PRE_DIV_RE.sub('', highlighted)
+        highlighted = DIV_PRE_RE.sub("", highlighted)
+        highlighted = PRE_DIV_RE.sub("", highlighted)
 
         starttag = self.starttag(
-            node, 'code', suffix='',
-            CLASS='docutils literal highlight highlight-%s' % lang
+            node,
+            "code",
+            suffix="",
+            CLASS="docutils literal highlight highlight-%s" % lang,
         )
-        self.body.append(starttag + highlighted + '</code>')
+        self.body.append(starttag + highlighted + "</code>")
 
     else:
         starttag = (
-            self.starttag(node, 'code', suffix='', CLASS='docutils literal') 
+            self.starttag(node, "code", suffix="", CLASS="docutils literal")
             + '<span class="pre">'
         )
-        self.body.append(starttag + node.astext() + '</span></code>')
+        self.body.append(starttag + node.astext() + "</span></code>")
 
     raise nodes.SkipNode
 
