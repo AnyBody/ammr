9432af36b79780f694dd342bdb95a2a667673114
cmdzialo <cmd@anybodytech.com>
2019-02-13 09:45:55 +0100

    -simplified version of KneeSimulator model -added medial and lateral tibial insert contact surfaces in order to calculate the medial, lateral contact forces -added some comments throughout
    Changed the images slightly for the knee simulator documentation.
    Added some text and fixed a couple typos in plot_KneeSimulator.py
    eliminated sphinxcontrib.bibtex in conf.py
    
    -renamed some STLs
    -added ifndef and if/else statements around  certain define statements to help test_kneesimulator run
    
    -moved images from   Applications/Orthopedics_and_rehab/ to Applications/images and converted images from png to jgp
    -added additional tests for test_KneeSimulator.any
    
    -fixed reiterative code in test_KneeSimulator.any and added switch to OperationSequences.any
    
    add gitkeep file to make sure this folder is remembered
    
    Got rid of  #define DEF_GAIT_NSTEP 3
    changing the DEF_GAIT_NSTEP to 3 causes model fail in step 1 of inverse dynamics.
    -moved insertion slightly of iMCPL and mMCPL to avoid wrapping warning
    
    fix typo

--- Docs/Applications/Orthopedics_and_rehab/plot_KneeSimulator.py
+++ Docs/Applications/Orthopedics_and_rehab/plot_KneeSimulator.py
@@ -3,12 +3,13 @@ r"""
 Knee Simulator Model
 =======================================
 
-Model of a Knee Simulator using a knee implant model and force-dependent kinematics (FDK).
+Model of a Knee Simulator using a knee implant model and force-dependent kinematics (FDK). 
+For an indepth description of the mathematics and mechanics behind FDK please see [AZDN17]_.
 
 **Main file:** ``Application/Examples/KneeSimulator/AnyKneeSimulator.Main.any``
 
 
-.. figure:: /Applications/Orthopedics_and_rehab/KneeSimulator_Full.png
+.. figure:: /Applications/images/KneeSimulator_Full.jpg
     :align: center
 
     Overview of the knee simulator
@@ -16,36 +17,38 @@ Model of a Knee Simulator using a knee implant model and force-dependent kinemat
 
 This is stand alone demo model of a knee simulator device resembling the principles of the Kansas Knee simulator [HCMT10]_.
 The model is contructed as a stand-alone model and doesn't use any elements and body parts from the model repository (AMMR). 
-Data for the knee implant comes from the `"6th Grand Challenge Competition to Predict In Vivo Knee Loads"<https://simtk.org/projects/kneeloads)>`__. 
+Data for the total knee replacement (TKR) implant comes from the `6th Grand Challenge Competition to Predict In Vivo Knee Loads <https://simtk.org/projects/kneeloads>`_. 
 
 
 Segments
 ---------------------
-The model is constructed with five main segments (femur, patella, tibia, ankle-fixture, hip-fixture). 
-In practice more segments are included. The mass of the femur/tibia is implmented as separate segments
-for easier specification of the moments of enertia. Likewise, the transspherical mechanism beween ankle
-and ground segments is implmented with three  segments connected with revolute and slider joints. 
+The model is constructed of five main segments (femur, patella, tibia, ankle-fixture, and hip-fixture). 
+In practice more segments are included. The mass of the femur/tibia is implemented as separate segments
+for easier specification of the moments of inertia. Likewise, the trans-spherical mechanism between ankle
+and ground segments is implmented using three segments connected with revolute and slider joints. 
 
 
 Ligaments
 -----------------------------
+The ligaments are modeled using one-dimensional nonlinear elastic spring elements. In order to prevent 
+ligaments from penetrating the bone/implant surfaces, they were wrapped around various geometrical shapes (cylinder, ellipsoid,...).
+Ligament properties (stiffness and reference strains) were adopted from the literature [BKHG91]_ & [MVFK15]_.
 
 
-
-.. figure:: /Applications/Orthopedics_and_rehab/KneeSimulator_Ligaments.png
+.. figure:: /Applications/images/KneeSimulator_Ligaments.jpg
     :align: center
 
-    Ligaments in the model
+    Ligaments in the model, abbreviations adopted from [SbTP06]_ Figure 1.
 
 
 
 Contacts
 --------------------------------
+Three STL-based rigid-rigid contact models were defined: between the patella and femoral component,  the femoral component and tibia insert (lateral side), 
+and the femoral component and tibia insert (medial side).
 
 
-
-
-.. figure:: /Applications/Orthopedics_and_rehab/KneeSimulator_Contacts.png
+.. figure:: /Applications/images/KneeSimulator_Contacts.jpg
     :align: center
 
     Contact surfaces in the model
@@ -56,39 +59,44 @@ Contacts
 
 Settings
 -------------------------
+Model settings can be added and removed here: ``UserDefinedSettings.Main.any`` to apply FDK to the tibiofemoral joint, patellofemoral joint, or both together.
 
+.. code-block:: AnyScriptDoc
 
+      #define USE_FDK_TIBIOFEMORAL
+      #define USE_FDK_PATELLOFEMORAL
 
+Other adjustable settings include: ligament parameters, femur and tibia mass ratios, load applied to the hip fixture, and desired knee flexion. 
 
+.. code-block:: AnyScriptDoc
 
+    #define DEF_KNEE_FLEXION_MIN 10
+    #define DEF_KNEE_FLEXION_MAX 60
 
+    #define DEF_HIP_AXIAL_LOAD_MIN 200
+    #define DEF_HIP_AXIAL_LOAD_MAX 200  
 
+    #define FEMUR_MASS_RATIO 0.135
+    #define TIBIA_MASS_RATIO 0.0465
 
+    #include "Input/Ligament_Properties.any"
 
-
-
-
-
-
-
-
-
-
-
-
-Please notice that this simulation is only a demo example. 
+Please note that this simulation is only a demo example.
 
 References
 -----------------------
 
+.. [AZDN17] Andersen, M. S., de Zee, M., Damsgaard, M., Nolte, D., & Rasmussen, J. Introduction to Force-Dependent Kinematics: Theory and Application 
+   to Mandible Modeling. J Biomech Eng. 139(9), 091001 (2017). doi: `10.1115/1.4037100 <https://doi.org/10.1115/1.4037100>`__
 .. [HCMT10] Halloran JP, Clary CW, Maletsky LP, Taylor M, Petrella AJ, Rullkoetter PJ. Verification 
    of predicted knee replacement kinematics during simulated gait in the Kansas knee simulator. J 
-   Biomech Eng. 2010; doi:`10.1115/1.4001678<http://dx.doi.org/10.1115/1.4001678>`__
-.. [SbTP06] Shelburne, K. B., Torry, M. R. & Pandy, M. G. 
-   Contributions of muscles, ligaments, and the ground-reaction force to tibiofemoral joint loading
-   during normal gait. J. Orthop. Res. 24, 1983–1990 (2006). doi:`10.1002/jor.20255 <http://dx.doi.org/10.1002/jor.20255>`__
+   Biomech Eng. 132(8), 081010 (2010). doi:`10.1115/1.4001678 <http://dx.doi.org/10.1115/1.4001678>`__
+.. [BKHG91] Blankevoort, L., Kuiper, J. H., Huiskes, R., and Grootenboer, H. J., “Articular Contact 
+   in a Three-Dimensional Model of the Knee,” J. Biomech., 24(11), pp. 1019–1031, (1991). doi:`10.1016/0021-9290(91)90019 <https://doi.org/10.1016/0021-9290(91)90019-J>`__  
 .. [MVFK15] Marra, M. A. et al. A subject-specific musculoskeletal modeling framework to predict in
    vivo mechanics of total knee arthroplasty. J. Biomech. Eng. 137, 020904 (2015). doi:`10.1115/1.4029258 <http://dx.doi.org/10.1115/1.4029258>`__
+.. [SbTP06] Shelburne, K. B., Torry, M. R. & Pandy, M. G. Contributions of muscles, ligaments, and the ground-reaction force to tibiofemoral joint loading
+   during normal gait. J. Orthop. Res. 24, 1983–1990 (2006). doi:`10.1002/jor.20255 <http://dx.doi.org/10.1002/jor.20255>`__
 
 
 
