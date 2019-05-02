c2655e6e92be9bfd61301bec73a34e17c9500a1e
Morten Enemark Lund <melund@gmail.com>
2019-05-02 13:38:02 +0200

    Add first version of Python util functions to AMMR

---
+++ Tools/ModelUtilities/Python/ams_python_utils.py
@@ -0,0 +1,37 @@
+import os
+from pathlib import Path
+from dataclasses import dataclass
+from typing import Tuple
+
+
+@dataclass
+class AMSContext:
+    py_file: str
+    fun_name: str
+    anyfun_full_name: str
+    anyfunex_file: str
+    anyfunex_line: int
+    call_location_file: str
+
+
+ACtx = Tuple[str, str, str, str, int, str]
+
+
+def os_path_exists(context: ACtx, fpath: str) -> int:
+    context = AMSContext(*context)
+    fpath = Path(fpath)
+    if not fpath.is_absolute():
+        fpath = Path(context.call_location_file).parent.joinpath(fpath)
+    return int(os.path.exists(fpath))
+
+
+def os_path_dirname(context: ACtx, fpath: str) -> str:
+    return os.path.dirname(fpath)
+
+
+def os_path_abspath(context: ACtx, fpath: str) -> str:
+    return os.path.abspath(fpath)
+
+
+def os_path_basename(context: ACtx, fpath: str) -> str:
+    return os.path.basename(fpath)
