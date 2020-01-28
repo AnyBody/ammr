0f5230da463649d0c254832d1ce5ccf61ec55dd5
Morten Enemark Lund <melund@gmail.com>
2020-01-28 17:27:31 +0100

    Updates to compare framework

--- Tests/run-comparison-test.bat
+++ Tests/run-comparison-test.bat
@@ -1,1 +1,1 @@
-pytest test_compare.py --h5compare-target=anytest-output\target --h5compare-reference=anytest-output\reference
+pytest test_compare.py --h5compare-target=anytest-output\ams7.3beta --h5compare-reference=anytest-output\reference
--- Tests/test_compare.py
+++ Tests/test_compare.py
@@ -6,6 +6,16 @@ except ImportError:
     pytestmark = pytest.mark.skipif(True, reason="Missing pytest_h5compare plugin.")
 
 
-def test_Pos(compare_regex):
+def test_Pos(compare_regex, make_anytest_record):
     failed, total = compare_regex(r".*Pos$", atol=1e-6, rtol=1e-8)
+    
+    for item in failed:
+        make_anytest_record(item)
+    make_anytest_record({"max_abs_err":1e-4})
     assert_compare_info(failed, total)
+
+
+# def test_Vel(compare_regex, make_anytest_record):
+#     failed, total = compare_regex(r".*Vel$", atol=1e-6, rtol=1e-8)
+#     map(make_anytest_record, failed)
+#     assert_compare_info(failed, total)
