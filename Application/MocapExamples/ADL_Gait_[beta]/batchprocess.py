cbddc97ca37114b560dc3578e9d0b5734813864e
Morten Enemark Lund <melund@gmail.com>
2020-08-14 13:33:57 +0200

    ADL gait: Update batch processing script

--- Application/MocapExamples/ADL_Gait_[beta]/batchprocess.py
+++ Application/MocapExamples/ADL_Gait_[beta]/batchprocess.py
@@ -1,36 +1,63 @@
+""" Script for batch processing all data in the walking dataset.
+   
+   The script runs batch processing in three steps:
+
+   1. Runs all the standing reference trials to 
+      calculate scaling and and marker positions for each subject. 
+
+   2. Runs marker tracking for all trials, saving joint angles to files. 
+
+   3. It runs the inverse dyanmic analysis for all trials.
+      This steps outputs the EKin variable. But can be extended
+      to output any variable from the model. 
+
+   Note: The script uses AnyPyTools, which is an open source library for 
+         working with the AnyBody Modeling System. If you use this library 
+         for publication please cite:
+
+         Lund et al., (2019). AnyPyTools: A Python package for reproducible 
+         research with the AnyBody Modeling System. Journal of Open Source 
+         Software, 4(33), 1108, https://doi.org/10.21105/joss.01108
+
 """
-   Example script for how to batch process trials in the model.
-
-   The script uses AnyPyTools, which is an open source library for 
-   working with the AnyBody Modeling System. If you use this library 
-   for publication please cite:
-
-   Lund et al., (2019). AnyPyTools: A Python package for reproducible 
-   research with the AnyBody Modeling System. Journal of Open Source 
-   Software, 4(33), 1108, https://doi.org/10.21105/joss.01108
-
-"""
+import os
 import glob
 import shutil
+import warnings
 
-from anypytools import AnyPyProcess
-from anypytools import macro_commands as mc
+try:
+    from anypytools import AnyPyProcess
+    from anypytools import macro_commands as mc
+except ImportError as exp:
+    msg = (
+        "AnyPyTools missing. Please install the AnyPyTools library to run batch processing: "
+        "https://anybody-research-group.github.io/anypytools-docs/"
+    )
+    raise ImportError(msg) from exp
 
 
-if not bool(glob.glob("c3dfiles/**/*.c3d")):
-    raise (
+# Check if the full dataset is present
+if len(glob.glob("c3dfiles/**/*.c3d")) < 3:
+    warnings.warn(
         "Dataset not present. Please see "
         "Schreiber & Moissenet (2019) https://doi.org/10.1038/s41597-019-0124-4 "
         "and download data from  https://ndownloader.figshare.com/files/14397965 "
     )
+
+# Create folder for storing batch processing logfiles
+# Note: Log files are automatically deleted unless the model fails.
+os.makedirs("BatchProcessingLogs", exist_ok=True)
 
 #%% Process all standing reference trials
 macro = [mc.Load("Main.any"), mc.OperationRun("Main.RunParameterIdentification")]
 
 app = AnyPyProcess()
 app.start_macro(
-    macro, search_subdirs=r"\d{7}_ST\\Main.any", logfile="ParameterOptLog.txt"
+    macro,
+    search_subdirs=r"\d{7}_ST\\Main.any",
+    logfile="BatchProcessingLogs/ParameterOptimization.txt",
 )
+
 
 #%% Process all marker tracking
 
@@ -42,24 +69,28 @@ macro = [
 
 app = AnyPyProcess()
 app.start_macro(
-    macro, search_subdirs=r"\d{7}_C\d_\d\d\\Main.any", logfile="MarkerTrackingLog.txt"
+    macro,
+    search_subdirs=r"\d{7}_C\d_\d\d\\Main.any",
+    logfile="BatchProcessingLogs/MarkerTracking.txt",
 )
 
 #%% Process all Inverse dynamics and export results
-
 
 macro = [
     mc.Load("Main.any"),
     mc.OperationRun("Main.RunAnalysis.LoadParameters"),
     mc.OperationRun("Main.RunAnalysis.InverseDynamics"),
     mc.Dump("Main.Studies.InverseDynamicStudy.Ekin"),
+    # Add more output here
 ]
 
 app = AnyPyProcess()
 results = app.start_macro(
-    macro, search_subdirs=r"\d{7}_C\d_\d\d\\Main.any", logfile="InverseDynamicsLog.txt"
+    macro,
+    search_subdirs=r"\d{7}_C\d_\d\d\\Main.any",
+    logfile="BatchProcessingLogs/InverseDynamics.txt",
 )
 
-# Please see 
+# Please see
 # https://anybody-research-group.github.io/anypytools-docs/Tutorial/01_Getting_started_with_anypytools.html
 # for how to work with the results from AnyPyTools
