93d9c706ac388749f4f1c50711b2674a354c42b2
Morten Lund <melund@gmail.com>
2021-02-04 13:05:37 +0100

    Fix syntax typo

--- Docs/exts/anylink.js
+++ Docs/exts/anylink.js
@@ -1,4 +1,4 @@
-function anylink_file(dummy_element, version, repo, target, relative_repo_path) {
+function anylink_file(dummy_element, version, repo, target, repo_relative_path) {
     href =  "anylink://AnyFile?"
     
     if (version != ""){
@@ -9,7 +9,7 @@ function anylink_file(dummy_element, version, repo, target, relative_repo_path) 
 
     if (window.location.protocol == "file:" && repo_relative_path != ""){
        current_dir = path.dirname(window.location.pathname)
-       repo_path = path.join(current_dir, DOCUMENTATION_OPTIONS.URL_ROOT, relative_repo_path)
+       repo_path = path.join(current_dir, DOCUMENTATION_OPTIONS.URL_ROOT, repo_relative_path)
        if (repo_path.startsWith("/") && repo_path[2] == ":"){
          // This is windows path so strip the leading "/"
          repo_path = repo_path.slice(1)
