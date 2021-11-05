2bc05cf8cfbf0d6dd98e3bddf64b2b4dbbb34ead
Enrico De Pieri <enrico.depieri@hest.ethz.ch>
2021-11-05 21:22:35 +0100

    Additional image in the documentation + edit torsional values in the example
    Added an image to the documentation to show the effect of the morphed torsion on muscle lines of action.  I'm not sure it is included correctly in the syntax of the doc.
    
    Changed the default value in the example to 20. This value is considered within the normal healthy range, but still it's quite different from the baseline of the TLEM cadaver. -5 would have been retroversion. It's less common and also pathological.
    
    Changed the reference value of the baseline model to 5.5 so it is consistent with the paper until a better way to calculate this is implemented.

--- Docs/Applications/Orthopedics_and_rehab/plot_FemoralTorsionTool.py
+++ Docs/Applications/Orthopedics_and_rehab/plot_FemoralTorsionTool.py
@@ -3,10 +3,10 @@ r"""
 Femoral Torsion tool
 =======================================
 
-This is an example of modifying the femoral torsion using a model utility tool
-included with the model repository. The  model is simple standing model where
-the femoral torsion has been altered to -5 degrees. The model is also a good 
-example on how a similar approach can be used for other body parts. 
+This is an example of modifying the femoral torsion of the generic TLEM geometry 
+by using a model utility tool included within the model repository. 
+The  model is the simple standing model where the femoral torsion has been altered to 25 degrees. 
+The model is also a good example on how a similar morphing approach can be used to personalize different body segments. 
 
 
 .. rst-class:: without-title 
@@ -16,17 +16,25 @@ example on how a similar approach can be used for other body parts.
   :menuselection:`Application --> Examples --> FemoralTorsion --> StandingModel.Main.any` 
 
 
-The tool was developed by Enrico De Peiri at University of Basel Children’s
-Hospital as part of his work on how hip joint loadings are influenced by femoral
-torsion which is published in 'Fronties in Bioengineering and Biotechnology'
-[DFLM21]_. Please cite his work if you use this tool. 
+The tool was developed by Enrico De Pieri, from the University of Basel Children’s Hospital (UKBB),
+and Morten E. Lund, from AnyBody Technology A/S.
+This tool was developed in order to generate personalized models that account for subject-specific values of femoral torsion,
+and investigate the effect of torsional alignment on hip loading. 
+This work is published in 'Frontiers in Bioengineering and Biotechnology' [DFLM21]_. 
+Please cite this article when using this tool. 
 
 
 .. figure:: /Applications/Orthopedics_and_rehab/femoral-torsion.png
    :align: center
 
    The tools works by adding a 8 control points around the hip center and knee center, which control a RBF scale function.
-   The control points are scaled witht the default model scaling with the femoral torsion added on top.
+   The control points are scaled with the default model scaling, while the femoral torsion is added on top.
+
+.. figure:: /Applications/Orthopedics_and_rehab/femoral-torsion_muscles.tiff
+   :align: center
+
+   The origin and insertion points of the muscles follow the morphed femoral geometry.
+
 
 The model tool is a simple include file located in the model repository. It can
 be included in any model using the following code:
@@ -35,16 +43,16 @@ be included in any model using the following code:
 
   // The femoral torsion tool expects the following variables to be defined 
   Main.HumanModel.Anthropometrics = {
-     AnyVar FemoralTorsionRight = -5;
-     AnyVar FemoralTorsionLeft = -5;
+     AnyVar FemoralTorsionRight = 20;
+     AnyVar FemoralTorsionLeft = 20;
   };
   
   // This line must be included before the "HumanModel".
   #include "<ANYBODY_PATH_MODELUTILS>/FemoralTorsion/FemoralTorsionInclude.any"
 
 
-.. note:: The current tibial torsion in the unscaled (TLEM 2.0 cadaver) model
-        approx. 5 degrees. So setting the femoral torsion to 5 degrees will not
+.. note:: The current femoral torsion in the unscaled (TLEM 2.0 cadaver) model geometry is
+        approx. 5.5 degrees. So setting the femoral torsion to 5.5 degrees will not
         modify the model.
 
 The tool uses a functionality override scaling for thigh segments. In this case
@@ -52,9 +60,7 @@ the include file sets ``#define CUSTOM_SCALING_Right_Thigh`` and ``#define
 CUSTOM_SCALING_Left_Thigh`` to override the default scaling for the right/left
 thigh. 
 
-.. seealso:: The tool uses a functionality override scaling for thigh segments. In this case
-   the include file sets ``#define CUSTOM_SCALING_Right_Thigh`` and ``#define
-   CUSTOM_SCALING_Left_Thigh``. See the :doc:`scaling tutorial <tutorials:Scaling/lesson4>` for more
+.. seealso:: See the :doc:`scaling tutorial <tutorials:Scaling/lesson4>` for more
    information on how to use the custom scaling functionality in the general
    case. 
 
@@ -63,7 +69,7 @@ References
 -----------------------
 
 .. [DFLM21] De Pieri E, Friesenbichler B, List R, Monn S, Casartelli NC, Leunig
-   M, et al. Subject-Specific Modeling of Femoral Torsion Influences the
+   M, Ferguson SJ. Subject-Specific Modeling of Femoral Torsion Influences the
    Prediction of Hip Loading During Gait in Asymptomatic Adults. Front Bioeng
    Biotechnol. 2021;9: 679360. doi:`10.3389/fbioe.2021.679360
    <https://doi.org/10.3389/fbioe.2021.679360>`__
@@ -78,3 +84,4 @@ import gallery
 
 gallery.plot("../images/FemoralTorsion.jpg")
 gallery.show()
+
