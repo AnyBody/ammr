7c9383ee3c4fa6038a5259b406239e600a9d5814
dsc <dsc@anybodytech.com>
2022-04-11 14:52:45 +0200

    Documentation of ExoExample JR: Work in Progress

---
+++ Docs/Applications/Other/plot_ExoConcept_BoxLift.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+r"""
+Exoskeleton concept model
+===============================
+
+Exoskeleton concept model.
+
+
+.. rst-class:: without-title
+.. seealso:: **Main file location in AMMR:** 
+
+  :menuselection:`Application --> Examples --> ExoskeletonConcept --> ExoConcept_BoxLift.Main.any`
+
+This example shows how to study different exoskeleton concepts on an activity. The model is 
+based on the `webcast <https://www.anybodytech.com/simulation-driven-conceptual-design-of-exoskeletons/>` 
+presented by Prof. John Rasmussen from Aalborg University on March 28, 2022.
+
+
+
+
+"""
+
+import sys
+sys.path.insert(0, '../../exts')
+import gallery
+
+gallery.plot("../images/ExoskeletonConcept_BoxLift.jpg")
+gallery.show()
