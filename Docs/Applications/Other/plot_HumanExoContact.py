4a530ae47c31e0073332c11a567e323a5c640ad7
dsc <dsc@anybodytech.com>
2022-04-13 12:33:11 +0200

    Update AMMR documentation page for Exo example DSC

--- Docs/Applications/Other/plot_HumanExoContact.py
+++ Docs/Applications/Other/plot_HumanExoContact.py
@@ -10,11 +10,11 @@ Human Exoskeleton contact model.
 .. rst-class:: without-title
 .. seealso:: **Main file location in AMMR:** 
 
-  :menuselection:`Application --> Examples --> HumanExoskeletonContact --> Main.any`
+  :menuselection:`Application --> Examples --> ExoskeletonElbowContact --> ExoskeletonElbow_Contact.Main.any`
 
 
 This example shows a simple exoskeleton and how to use the conditional contact
-elements to set the kinetic connections between the human and the exoskeleton.
+elements to define the kinetic connections between the human and the exoskeleton.
 The example is based on the Standing Model. The mannequin drivers are used to
 syntehtically drive the arms of the model while it supports external loads at 
 the hands. An exoskeleton is added on to the right arm to assist elbow flexion. 
@@ -37,5 +37,5 @@ import sys
 sys.path.insert(0, '../../exts')
 import gallery
 
-gallery.plot("../images/HumanExoskeletonContact.jpg")
+gallery.plot("../images/ExoskeletonElbow_Contact.jpg")
 gallery.show()
