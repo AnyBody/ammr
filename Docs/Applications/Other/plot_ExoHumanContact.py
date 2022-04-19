fb5f72d0e007bebb31b9492b6404d8246636f19a
dsc <dsc@anybodytech.com>
2022-04-19 16:31:28 +0200

    Rename Files to be consistent with existing exo concept example and update test files, documentation accordingly.

--- Docs/Applications/Other/plot_ExoHumanContact.py
+++ Docs/Applications/Other/plot_ExoHumanContact.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 r"""
-Human Exoskeleton Contact
+Exoskeleton Human Contact
 ===============================
 
-Human Exoskeleton contact model.
+Exoskeleton Human contact model.
 
 
 
 .. rst-class:: without-title
 .. seealso:: **Main file location in AMMR:** 
 
-  :menuselection:`Application --> Examples --> ExoskeletonElbowContact --> ExoskeletonElbow_Contact.Main.any`
+  :menuselection:`Application --> Examples --> ExoskeletonContactElbow --> ExoskeletonContact_Elbow.Main.any`
 
 
 This example shows a simple exoskeleton and how to use the conditional contact
 elements to define the kinetic connections between the human and the exoskeleton.
 The example is based on the Standing Model. The mannequin drivers are used to
-syntehtically drive the arms of the model while it supports external loads at 
+synthetically drive the arms of the model while it supports external loads at 
 the hands. An exoskeleton is added on to the right arm to assist elbow flexion. 
 
 
@@ -37,5 +37,5 @@ import sys
 sys.path.insert(0, '../../exts')
 import gallery
 
-gallery.plot("../images/ExoskeletonElbow_Contact.jpg")
+gallery.plot("../images/ExoskeletonContact_Elbow.jpg")
 gallery.show()
