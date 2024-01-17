5a975735b68dbaac36b2bb63602b4a9644f03b6c
Morten Enemark Lund <melund@gmail.com>
2024-01-17 09:13:04 +0100

    Add user agent for link check

--- Docs/conf.py
+++ Docs/conf.py
@@ -450,6 +450,8 @@ import generate_class_template_docs
 generate_class_template_docs.run_all()
 
 
+# Ensure the link check is not blocked by the servers
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
 linkcheck_ignore = [
     r".*linkcheck_ignore",
